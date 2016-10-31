11bacb43e40839bedcb6193fc0b419c5cdafa40c
Gediminas Siutilas <gadominas@gmail.com>
2016-10-31 22:47:27 +0200

    crawlermock up

--- crawlerMock/run.sh
+++ crawlerMock/run.sh
@@ -9,6 +9,10 @@ start_time=`date +%s`
   "./crawlerMockInstanceNoClose.sh http://facebook.com $1" \
   "./crawlerMockInstance.sh http://twitter.com $1" \
   "./crawlerMockInstance.sh http://hak5.com $1" \
+  "./crawlerMockInstance.sh http://1.com $1" \
+  "./crawlerMockInstanceNoClose.sh http://wikileaks.org $1" \
+  "./crawlerMockInstance.sh http://wikileaks.org $1" \
+  "./crawlerMockInstance.sh https://facebookcorewwwi.onion $1" \
 
 end_time=`date +%s`
 echo execution time was `expr $end_time - $start_time` s.
